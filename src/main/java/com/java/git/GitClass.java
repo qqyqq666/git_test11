25c96e77da83f33d30d7afae550d2da5da24a127
JLX <619329231@qq.com>
2021-08-07 08:33:24 +0800

    拿来吧你

---
+++ src/main/java/com/java/git/GitClass.java
@@ -0,0 +1,11 @@
+package com.java.git;
+
+/**
+ * @author JlX
+ * @create 2021-08-07 8:29
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
